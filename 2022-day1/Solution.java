12b200801e8e371c8a90784d57a25cbfa9d3002f
Yen-Ming Lee <leeym@leeym.com>
2022-12-04 22:25:26 -0800

    2022-day1

--- 2022-day1/Solution.java
+++ 2022-day1/Solution.java
@@ -7,10 +7,10 @@ import java.util.*;
 
 public class Solution {
 
-    Queue<String> readFile() {
+    Queue<String> readFile(String pathname) {
         Queue<String> queue = new LinkedList<>();
         try {
-            Scanner scanner = new Scanner(new File("input"));
+            Scanner scanner = new Scanner(new File(pathname));
             while (scanner.hasNextLine())
                 queue.add(scanner.nextLine());
             scanner.close();
@@ -20,9 +20,8 @@ public class Solution {
         return queue;
     }
 
-    @Test
-    public void part1() {
-        Queue<String> file = readFile();
+    public int part1(String pathname) {
+        Queue<String> file = readFile(pathname);
         int sum = 0;
         int max = 0;
         while (!file.isEmpty()) {
@@ -34,12 +33,11 @@ public class Solution {
                 sum = 0;
             }
         }
-        System.out.println(max);
+        return max;
     }
 
-    @Test
-    public void test2() {
-        Queue<String> file = readFile();
+    public int part2(String pathname) {
+        Queue<String> file = readFile(pathname);
         Queue<Integer> queue = new PriorityQueue<>(Comparator.naturalOrder());
         int sum = 0;
         while (!file.isEmpty()) {
@@ -53,6 +51,26 @@ public class Solution {
                 sum = 0;
             }
         }
-        System.out.println(queue.stream().mapToInt(Integer::intValue).sum());
+        return queue.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    @Test
+    public void small1() {
+        Assert.assertEquals(24000, part1("small"));
+    }
+
+    @Test
+    public void large1() {
+        Assert.assertEquals(69883, part1("large"));
+    }
+
+    @Test
+    public void small2() {
+        Assert.assertEquals(45000, part2("small"));
+    }
+
+    @Test
+    public void large2() {
+        Assert.assertEquals(207576, part2("large"));
     }
 }
